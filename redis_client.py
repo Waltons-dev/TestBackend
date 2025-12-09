926c3133a469029cb6ba59b63e7bdfb6882ab2e7
KirillZ <zahvatinkirill@mail.ru>
2025-12-10 01:21:17 +0300

    func name changes

--- redis_client.py
+++ redis_client.py
@@ -3,9 +3,9 @@ import json
 
 r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
-def save(key, data):
+def save_fundamental(key, data):
     r.set(key, json.dumps(data, ensure_ascii=False))
 
-def load(key):
+def load_fundamental(key):
     value = r.get(key)
     return json.loads(value) if value else None
