d135f748d3183a085d4ae31d2c64d98440faa392
KirillZ <zahvatinkirill@mail.ru>
2025-12-10 01:22:47 +0300

    del not working option

--- selenium_driver.py
+++ selenium_driver.py
@@ -9,6 +9,5 @@ def init_driver():
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-extensions")
-    options.add_argument("--headless=new")
     driver = webdriver.Chrome(service=service, options=options)
     return driver
