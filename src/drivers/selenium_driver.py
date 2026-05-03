3dac81860c1bc02b556b8ad8b1fcf2c8fa09816a
KirillZ <zahvatinkirill@mail.ru>
2026-05-03 21:37:31 +0300

    feat: create remote webriver

--- src/drivers/selenium_driver.py
+++ src/drivers/selenium_driver.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import os
 
 def init_driver():
-    service = Service(ChromeDriverManager().install())
-    options = webdriver.ChromeOptions()
+    options = Options()
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-extensions")
-    driver = webdriver.Chrome(service=service, options=options)
+    
+    selenium_url = os.getenv("SELENIUM_REMOTE_URL", "http://localhost:4444/wd/hub")
+    driver = webdriver.Remote(
+        command_executor=selenium_url,
+        options=options
+    )
+    
     return driver
